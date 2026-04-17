5826695bb36f9695c58038a387e065b4d0d8498b
leo-younger <232172629+leo-younger@users.noreply.github.com>
2026-04-18 01:35:09 +0800

    feat: 完善归并排序实现并重构测试框架
    1. 修复归并排序算法实现错误
    2. 新增归并排序对应的测试类 MergeSortTest
    3. 提取测试通用逻辑到 SortTestUtils 工具类
    4. 重构所有排序测试类，统一使用工具类进行测试

--- src/test/java/com/sort/BubbleSortTest.java
+++ src/test/java/com/sort/BubbleSortTest.java
@@ -1,35 +1,18 @@
 package com.sort;
 
-import java.util.Random;
-
 import static com.sort.BubbleSort.bubbleSort;
-import static java.lang.System.currentTimeMillis;
+
 
 public class BubbleSortTest {
     public static void main(String[] args)
         {
-            //创建随机数组
-        int[] bubbleSortArr = new int[30];
-        Random random = new Random();
-        for (int i = 0; i < bubbleSortArr.length; i++) {
-            bubbleSortArr[i] = random.nextInt(100);
-
-        }
-        //打印排序前数组
-        for (int k : bubbleSortArr) {
-            System.out.print(k + " ");
-        }
-        System.out.println();
-
-        //调用函数并输出运行时间
-        long start = currentTimeMillis();
-        bubbleSort(bubbleSortArr);
-        long end = currentTimeMillis();
-        System.out.println(end - start);
-
-        //打印排序后的数组
-        for (int j : bubbleSortArr) {
-            System.out.print(j + " ");
-        }
+            //创建数组
+            int[] bubbleSortArr = SortTestUtils.createRandomArray(30,100);
+            //打印排序前数组
+            SortTestUtils.printArray(bubbleSortArr);
+            //对数组进行排序
+            SortTestUtils.testSort(bubbleSortArr, (ints)-> bubbleSort(ints));
+            //打印排序后的数组
+            SortTestUtils.printArray(bubbleSortArr);
         }
 }
--- src/test/java/com/sort/InsertionSortTest.java
+++ src/test/java/com/sort/InsertionSortTest.java
@@ -1,32 +1,18 @@
 package com.sort;
 
-import java.util.Random;
-
 import static com.sort.InsertionSort.insertionSort;
 
-import static java.lang.System.currentTimeMillis;
 
 public class InsertionSortTest {
     public static void main(String[] args)
         {
-        int[] InsertionSortArr = new int[30];
-        Random random = new Random();
-        for (int i = 0; i < InsertionSortArr.length; i++) {
-            InsertionSortArr[i] = random.nextInt(100);
-
-        }
-        for (int k : InsertionSortArr) {
-            System.out.print(k + " ");
-        }
-        System.out.println();
-
-        long start = currentTimeMillis();
-        insertionSort(InsertionSortArr);
-        long end = currentTimeMillis();
-        System.out.println(end - start);
-
-        for (int j : InsertionSortArr) {
-            System.out.print(j + " ");
-        }
+        //创建数组
+        int[] insertionSortArr = SortTestUtils.createRandomArray(30,100);
+        //打印排序前数组
+        SortTestUtils.printArray(insertionSortArr);
+        //对数组进行排序
+        SortTestUtils.testSort(insertionSortArr, (ints)-> insertionSort(ints));
+        //打印排序后的数组
+        SortTestUtils.printArray(insertionSortArr);
         }
 }
