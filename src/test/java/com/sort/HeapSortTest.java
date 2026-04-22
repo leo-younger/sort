dc3d7731d5d66de2c798266a122d07aa3cf0a6f9
leo-younger <232172629+leo-younger@users.noreply.github.com>
2026-04-22 23:03:27 +0800

    feat:新增堆排序测试类

--- src/test/java/com/sort/HeapSortTest.java
+++ src/test/java/com/sort/HeapSortTest.java
@@ -1,4 +1,18 @@
 package com.sort;
 
+import static com.sort.BubbleSort.bubbleSort;
+import static com.sort.HeapSort.heapSort;
+
 public class HeapSortTest {
+    public static void main(String[] args)
+        {
+        //创建数组
+        int[] heapSortArr = SortTestUtils.createRandomArray(30,100);
+        //打印排序前数组
+        SortTestUtils.printArray(heapSortArr);
+        //对数组进行排序
+        SortTestUtils.testSort(heapSortArr, ints-> heapSort(ints));
+        //打印排序后的数组
+        SortTestUtils.printArray(heapSortArr);
+        }
 }
